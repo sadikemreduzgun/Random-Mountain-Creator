5a9069c093e358dcd767a2a310e6068e7d9e621e
Sadık Emre <70113249+sadikemreduzgun@users.noreply.github.com>
2023-03-03 07:59:26 +0000

    Update create_hills.py

--- src/create_hills.py
+++ src/create_hills.py
@@ -7,27 +7,36 @@ from assign_location import random_locate
 def hill_type_1(area,locx=random_locate()[0], locy=random_locate()[1]):
     # DEFINE REQUIRED VARIABLES
     side_y = 10  # also side y determines how big the structure is and changeable
+    # max rise of mountain in every step, it is random and this is max
     limited_increase = 3
+    # assign mountain area
     main_list = numpy.zeros((side_y, side_y))
+    # assign the index which will be used in loop
     loop_index = 1
+    # hold the index
     hold_loop_index = loop_index
+    # determine if it is a sinkhole or mountain
     carry_mountain = 0
     carry_sinkhole = 0
+    # a random variable to randomly decide if main mountain is mountain or a sinkhole
     random_is_it_mountain = random.randint(0, 9)
-
+    # randomly decide if random var. is 2, make it a sinkhole. Unless make it a mountain
     if random_is_it_mountain == 2:
         main_list[loop_index][loop_index] += -1 * random.randint(0, limited_increase)
     else:
         main_list[loop_index][loop_index] += random.randint(0, limited_increase)
-
+        
+    # start main loop looping
     while loop_index < hold_loop_index + side_y / 2:
-
+        # assign x,y indexes
         x_number_index = loop_index
         y_number_index = loop_index
-
+        
+        # define max num for structure
         max_num_for_approach_in_loop = side_y - loop_index
 
         positive = 0
+        # understand if it is a mountain or a sinkhole
         if loop_index == 1:
             if main_list[loop_index][loop_index] > 0:
                 positive = 1
@@ -38,8 +47,8 @@ def hill_type_1(area,locx=random_locate()[0], locy=random_locate()[1]):
                 carry_mountain += 1
             else:
                 carry_sinkhole += 1
-
-        # 4 while loops is a loop index
+        
+        # assign corners of structure to be used further
         if loop_index != 1:
             if positive:
                 main_list[loop_index][loop_index] = random.randint(main_list[loop_index - 1][loop_index - 1] - 1,
@@ -50,6 +59,7 @@ def hill_type_1(area,locx=random_locate()[0], locy=random_locate()[1]):
                                                                    [loop_index - 1][loop_index - 1] - limited_increase,
                                                                    main_list[loop_index - 1][loop_index - 1] + 1)
 
+        # go up ^ while end point is not reached
         while y_number_index - 1 < max_num_for_approach_in_loop:
 
             n = main_list[loop_index][loop_index]
