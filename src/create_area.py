10d6d4f0e86b999609af26a4955cc742bb9a4759
Sadık Emre <70113249+sadikemreduzgun@users.noreply.github.com>
2023-05-08 14:02:20 +0300

    Update create_area.py

--- src/create_area.py
+++ src/create_area.py
@@ -3,9 +3,11 @@ import random as rd
 
 
 def create_area(area):
-
+    # for the number of nested lists in area
     for out in range(len(area)):
+        # for number of elemnts in a list in the area
         for i in range(len(area[0])):
+            # create a chaos, don't leave any flat area
             if area[out][i] == 0:
                 area[out][i] = rd.randint(0, 2)
 
